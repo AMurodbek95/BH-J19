3bf923967197f545174ef6f923926a5e5f8f1a5d
Murodbek <dmurodbek9595@gmail.com>
2019-12-10 21:56:36 +0300

    2nd commit

--- src/by/belhard/j19/lessons/lesson2/Main.java
+++ src/by/belhard/j19/lessons/lesson2/Main.java
@@ -1,15 +1,17 @@
 package by.belhard.j19.lessons.lesson2;
 
 public class Main {
-    public static void main (String [] args ){
+    public static void main(String[] args) {
 /*short sh =5;
 int i=100;
 double d =3.14;
 System.out.println(sh+i/d);
 */
-int i=0;
- System.out.println(i++);
- System.out.println(++i);
+        int i = 0;
+        System.out.println(i++);
+        System.out.println(++i);
+
+        System.out.println(i);
     }
 
 }
