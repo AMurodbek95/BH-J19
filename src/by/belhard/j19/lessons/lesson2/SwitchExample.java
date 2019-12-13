dc329b3d7e83fb5a91d1cc40ce7f99a036df3ec3
Murodbek <dmurodbek9595@gmail.com>
2019-12-13 21:23:28 +0300

    lassan 3

--- src/by/belhard/j19/lessons/lesson2/SwitchExample.java
+++ src/by/belhard/j19/lessons/lesson2/SwitchExample.java
@@ -1,7 +1,7 @@
 package by.belhard.j19.lessons.lesson2;
 
 public class SwitchExample {
-    String inputData ="mon";
+  /*  String inputData ="mon";
     switch (inputData)
    case "";
    System.out.println();
@@ -17,6 +17,6 @@ public class SwitchExample {
    String inputData ="mon";
    case "";
    System.out.println();
-   String inputData ="mon";
+   String inputData ="mon";*/
 
 }
