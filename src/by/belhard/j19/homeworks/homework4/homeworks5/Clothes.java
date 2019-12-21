58697768b3c15a2f461cccdc73f47276a7bafa7f
Murodbek <dmurodbek9595@gmail.com>
2019-12-21 18:19:37 +0300

    wardrobe

---
+++ src/by/belhard/j19/homeworks/homework4/homeworks5/Clothes.java
@@ -0,0 +1,14 @@
+package by.belhard.j19.homeworks.homework4.homeworks5;
+
+public class Clothes {
+    private String title;
+
+    public Clothes(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
